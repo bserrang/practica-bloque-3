1f89762a5abc8cd8f5d4e6808000c0c2914eceab
bserrang <bserrang@everis.com>
2020-11-06 21:55:07 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-// En esta práctica debéis crear una interfaz con el nombre "UserData" en el archivo "user-data.ts". En esta interfaz, debéis añadir las propiedades "name", "age" y "phone". Los tipos de estas tres propiedades son cadena de texto, número y número respectivamente. Tenéis que exportar esta interfaz.
+// En esta práctica debéis crear una interfaz con el nombre "UserData" en el archivo "user-data.ts". En esta interfaz, debéis añadir las propiedades "name", "age" y "phone" con los tipos cadena de texto, número y número respectivamente. Tenéis que exportar esta interfaz.
 // En el documento "index.ts" debéis importar la interfaz que habéis creado en el punto anterior. Cread una variable llamada "user" que sea del tipo de la interfaz que habéis importado.
-// Luego, debéis crear una función llamada "getPhone" que reciba un parámetro de tipo genérico. La idea es que esta función reciba un objeto de tipo "UserData" así que la funcionalidad de esta función va consistir en sacar por consola el teléfono del usuario.
-// Para terminar, asignad un objeto literal (sin clase) a la variable "user" (con los datos que queráis pero que no de erroes de tipo) y luego pasad por parámetros dicha variable a una llamada de la función "getPhone" (no olvidéis el genérico).
+// Luego, debéis crear una función llamada "getData" que reciba un tipo genérico, un parámetro del tipo genérico y que saque por consola ese dato.
+// Para terminar, asignad un objeto literal (sin clase) a la variable "user" (con los datos que queráis pero que no dé erroes de tipo) y luego realizad dos llamadas a "getData". La primera llamada debe recibir por parámetros el valor de la propiedad "name" del usuario y la segunda el valor de "phone".
+// OPCIONAL: Dentro de "getData" añadid un condicional con una guarda para diferenciar cuando reciba un tipo "number" y sacar por consola mensajes distintos para las dos llamadas anteriormente mencionadas.
 // Ánimo que ya lo tenéis!
